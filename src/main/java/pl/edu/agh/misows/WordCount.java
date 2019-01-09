830491c460fd7f1239bbe4ab751312c7957baf85
Marcin Lason <marcin.lason1@gmail.com>
2019-01-09 20:43:43 +0100

    Add summing words

--- src/main/java/pl/edu/agh/misows/WordCount.java
+++ src/main/java/pl/edu/agh/misows/WordCount.java
@@ -41,19 +41,27 @@ public class WordCount {
         }
     }
 
-    public static class CountWords extends PTransform<PCollection<String>, PCollection<KV<String, Long>>> {
+    public static class CountWords extends PTransform<PCollection<String>, PCollection<String>> {
         @Override
-        public PCollection<KV<String, Long>> expand(PCollection<String> lines) {
+        public PCollection<String> expand(PCollection<String> lines) {
             PCollection<String> words = lines.apply(ParDo.of(new ExtractWordsFn()));
-            PCollection<KV<String, Long>> wordCounts = words.apply(Count.perElement());
-            return wordCounts;
+            PCollection<Long> wordCounts = words.apply(Count.globally());
+            PCollection<String> stringWordCounts = wordCounts.apply(ParDo.of(new DoFn<Long, String>() {
+                @ProcessElement
+                public void processElement(@Element Long element, OutputReceiver<String> receiver) {
+                    if (Objects.nonNull(element)) {
+                        receiver.output(element.toString());
+                    }
+                }
+            }));
+            return stringWordCounts;
         }
     }
 
-    public static class CountVulgarWords extends PTransform<PCollection<String>, PCollection<KV<String, Long>>> {
+    public static class CountVulgarWords extends PTransform<PCollection<String>, PCollection<String>> {
         private static final List<String> vulgarWords = Arrays.asList("2g1c","2 girls 1 cup","acrotomophilia","alabama hot pocket","alaskan pipeline","anal","anilingus","anus","apeshit","arsehole","ass","asshole","assmunch","auto erotic","autoerotic","babeland","baby batter","baby juice","ball gag","ball gravy","ball kicking","ball licking","ball sack","ball sucking","bangbros","bareback","barely legal","barenaked","bastard","bastardo","bastinado","bbw","bdsm","beaner","beaners","beaver cleaver","beaver lips","bestiality","big black","big breasts","big knockers","big tits","bimbos","birdlock","bitch","bitches","black cock","blonde action","blonde on blonde action","blowjob","blow job","blow your load","blue waffle","blumpkin","bollocks","bondage","boner","boob","boobs","booty call","brown showers","brunette action","bukkake","bulldyke","bullet vibe","bullshit","bung hole","bunghole","busty","butt","buttcheeks","butthole","camel toe","camgirl","camslut","camwhore","carpet muncher","carpetmuncher","chocolate rosebuds","circlejerk","cleveland steamer","clit","clitoris","clover clamps","clusterfuck","cock","cocks","coprolagnia","coprophilia","cornhole","coon","coons","creampie","cum","cumming","cunnilingus","cunt","darkie","date rape","daterape","deep throat","deepthroat","dendrophilia","dick","dildo","dingleberry","dingleberries","dirty pillows","dirty sanchez","doggie style","doggiestyle","doggy style","doggystyle","dog style","dolcett","domination","dominatrix","dommes","donkey punch","double dong","double penetration","dp action","dry hump","dvda","eat my ass","ecchi","ejaculation","erotic","erotism","escort","eunuch","faggot","fecal","felch","fellatio","feltch","female squirting","femdom","figging","fingerbang","fingering","fisting","foot fetish","footjob","frotting","fuck","fuck buttons","fuckin","fucking","fucktards","fudge packer","fudgepacker","futanari","gang bang","gay sex","genitals","giant cock","girl on","girl on top","girls gone wild","goatcx","goatse","god damn","gokkun","golden shower","goodpoop","goo girl","goregasm","grope","group sex","g-spot","guro","hand job","handjob","hard core","hardcore","hentai","homoerotic","honkey","hooker","hot carl","hot chick","how to kill","how to murder","huge fat","humping","incest","intercourse","jack off","jail bait","jailbait","jelly donut","jerk off","jigaboo","jiggaboo","jiggerboo","jizz","juggs","kike","kinbaku","kinkster","kinky","knobbing","leather restraint","leather straight jacket","lemon party","lolita","lovemaking","make me come","male squirting","masturbate","menage a trois","milf","missionary position","motherfucker","mound of venus","mr hands","muff diver","muffdiving","nambla","nawashi","negro","neonazi","nigga","nigger","nig nog","nimphomania","nipple","nipples","nsfw images","nude","nudity","nympho","nymphomania","octopussy","omorashi","one cup two girls","one guy one jar","orgasm","orgy","paedophile","paki","panties","panty","pedobear","pedophile","pegging","penis","phone sex","piece of shit","pissing","piss pig","pisspig","playboy","pleasure chest","pole smoker","ponyplay","poof","poon","poontang","punany","poop chute","poopchute","porn","porno","pornography","prince albert piercing","pthc","pubes","pussy","queaf","queef","quim","raghead","raging boner","rape","raping","rapist","rectum","reverse cowgirl","rimjob","rimming","rosy palm","rosy palm and her 5 sisters","rusty trombone","sadism","santorum","scat","schlong","scissoring","semen","sex","sexo","sexy","shaved beaver","shaved pussy","shemale","shibari","shit","shitblimp","shitty","shota","shrimping","skeet","slanteye","slut","s&m","smut","snatch","snowballing","sodomize","sodomy","spic","splooge","splooge moose","spooge","spread legs","spunk","strap on","strapon","strappado","strip club","style doggy","suck","sucks","suicide girls","sultry women","swastika","swinger","tainted love","taste my","tea bagging","threesome","throating","tied up","tight white","tit","tits","titties","titty","tongue in a","topless","tosser","towelhead","tranny","tribadism","tub girl","tubgirl","tushy","twat","twink","twinkie","two girls one cup","undressing","upskirt","urethra play","urophilia","vagina","venus mound","vibrator","violet wand","vorarephilia","voyeur","vulva","wank","wetback","wet dream","white power","wrapping men","wrinkled starfish","xx","xxx","yaoi","yellow showers","yiffy","zoophilia");
         @Override
-        public PCollection<KV<String, Long>> expand(PCollection<String> lines) {
+        public PCollection<String> expand(PCollection<String> lines) {
             PCollection<String> words = lines.apply(ParDo.of(new ExtractWordsFn()));
             PCollection<String> filteredWords = words.apply(ParDo.of(new DoFn<String, String>() {
                 @ProcessElement
@@ -63,8 +71,16 @@ public class WordCount {
                         receiver.output(element);
                 }
             }));
-            PCollection<KV<String, Long>> wordCounts = filteredWords.apply(Count.perElement());
-            return wordCounts;
+            PCollection<Long> wordCounts = filteredWords.apply(Count.globally());
+            PCollection<String> stringWordCounts = wordCounts.apply(ParDo.of(new DoFn<Long, String>() {
+                @ProcessElement
+                public void processElement(@Element Long element, OutputReceiver<String> receiver) {
+                    if (Objects.nonNull(element)) {
+                        receiver.output(element.toString());
+                    }
+                }
+            }));
+            return stringWordCounts;
         }
     }
 
@@ -91,12 +107,12 @@ public class WordCount {
         }
     }
 
-    static void runWordCount(WordCountOptions options, PTransform<PCollection<String>, PCollection<KV<String, Long>>> transform) {
+    static void runWordCount(WordCountOptions options, PTransform<PCollection<String>, PCollection<String>> transform) {
         Pipeline p = Pipeline.create(options);
 
         p.apply("ReadLines", TextIO.read().from(options.getInputFile()))
                 .apply(transform)
-                .apply(MapElements.via(new FormatAsTextFn()))
+//                .apply(MapElements.via(new FormatAsTextFn()))
                 .apply("WriteCounts", TextIO.write().to(options.getOutput()));
         p.run().waitUntilFinish();
     }
